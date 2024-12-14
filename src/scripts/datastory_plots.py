7e047e4a276e6e4cb3510b74798cea06b4fdc1d9
TancrediCogne <75041551+TancrediCogne@users.noreply.github.com>
2024-12-14 18:34:47 +0100

    update on scripts

--- src/scripts/datastory_plots.py
+++ src/scripts/datastory_plots.py
@@ -9,7 +9,9 @@ from rdkit.Chem import AllChem
 import nglview as nv
 import py3Dmol
 import plotly.graph_objects as go
+import torch
 from src.scripts.mutants_analysis import *
+from plotly.subplots import make_subplots
 
 
 def get_ligand_name_from_smiles(smiles):
@@ -36,27 +38,33 @@ def save_dfs_ligands(df_merged, df_mutants):
     :return: interaction pair names of the structure [Ligand SMILES, Uniprot Name of WT]
     """
     saving_folder_dfs = "../data/pair_dfs"
+    saving_folder_dfs_test = "../data/test"
     pair_numbers = 0
-    interaction_pair_names = []
+    ligand_smiles = []
+    wt_names= []
 
     # Filtering pairs and saving dfs and ligand infos
     for _, row in df_mutants.iterrows():
         # Filter to keep only interaction pairs with at least 10 mutants
         if len(row['Target Names']) > 10:
-            df_pair = compute_variation_ic50(row, df_merged)
+            df_pair, test = compute_variation_ic50(row, df_merged)
             if df_pair is None:
                 # See P2 first cell for explanation of this problem TODO
                 print("This pair will not be saved due to multiple conflicting values in BindingDB") 
             else: 
                 pair_numbers += 1
-                interaction_pair_names.append([row['Ligand SMILES'], row['UniProt (SwissProt) Entry Name of Target Chain']])
+                ligand_smiles.append(row['Ligand SMILES'])
+                wt_names.append(row['UniProt (SwissProt) Entry Name of Target Chain'])
                 # Saving df to csv file for easier access later on
-                saving_path_df = os.path.join(saving_folder_dfs, f'/interaction_pair{pair_numbers}.csv')
+                saving_path_df = os.path.join(saving_folder_dfs, f'interaction_pair{pair_numbers}.csv')
                 df_pair.to_csv(saving_path_df)
+                saving_path_df_test = os.path.join(saving_folder_dfs_test, f'interaction_pair{pair_numbers}_test.csv')
+                test.to_csv(saving_path_df_test)
                 print("Pair information succesfully saved")
             print("---------------------------------------------------------------------------")
 
-    return interaction_pair_names
+    ligand_names = [get_ligand_name_from_smiles(s) for s in ligand_smiles]
+    return pd.DataFrame({'Ligand SMILES': ligand_smiles, 'Ligand name': ligand_names, 'WT protein': wt_names})
 
 def visualize_ligands(all_smiles):
     """
@@ -66,12 +74,8 @@ def visualize_ligands(all_smiles):
     """
 
     saving_folder_ligands = "../plots/ligands"
-    ligand_names = []
 
     for idx, smiles in enumerate(all_smiles):
-        ligand_name = get_ligand_name_from_smiles(smiles)
-        ligand_names.append(ligand_name)
-
         # Creating and Saving Ligand Representation from SMILES
         mol = Chem.MolFromSmiles(smiles)
 
@@ -90,11 +94,22 @@ def visualize_ligands(all_smiles):
         viewer.setStyle({'stick': {}})
         viewer.setBackgroundColor('white')
 
+        min_coords = [min(coord[i] for coord in coords) for i in range(3)]
+        max_coords = [max(coord[i] for coord in coords) for i in range(3)]
+
+        viewer.zoomTo({
+            'center': {
+                'x': (min_coords[0] + max_coords[0]) / 2,
+                'y': (min_coords[1] + max_coords[1]) / 2,
+                'z': (min_coords[2] + max_coords[2]) / 2,
+            },
+            'zoom': 0.5,  # Adjust zoom level if needed
+        })
+
         # Saving ligand representation
         saving_path_ligand = os.path.join(saving_folder_ligands, f'ligand_{idx}')
-        viewer.write_html(saving_path_ligand + '.html')
 
-    return ligand_names
+        viewer.write_html(saving_path_ligand + '.html')
 
 def generate_interactive_ic50_plot():
     """
@@ -102,73 +117,97 @@ def generate_interactive_ic50_plot():
     """
 
     file_directory = '../data/pair_dfs'
-    saving_directory = 'src/plots'
+    saving_directory = '../plots'
 
     buttons = []
     all_traces = []
     total_number_of_mutants_to_display = 0
-
     for file_name in os.listdir(file_directory):
-        path = os.path.join(file_directory, file_name)
-        df_pair = pd.read_csv(path)
-        total_number_of_mutants_to_display += len(df_pair['Mutant Name'].unique())
+        if file_name.endswith('.csv'):
+            path = os.path.join(file_directory, file_name)
+            df_pair = pd.read_csv(path)
 
+            total_number_of_mutants_to_display += len(df_pair['Mutant Name'].unique())
+
+    fig = make_subplots(rows=2, cols=1, shared_xaxes=True, shared_yaxes=True, x_title='Mutation Position', y_title='Log Ratio of IC50 values between WT and a given mutant')
     # Going over every saved interaction df
     for idx, file_name in enumerate(os.listdir(file_directory)):
-        path = os.path.join(file_directory, file_name)
-        df_pair = pd.read_csv(path)
+        if file_name.endswith('.csv'):
+            path = os.path.join(file_directory, file_name)
 
-        # Add traces for the current file
-        visibility = []
-        df_pair['Mutant Name'] = df_pair['Mutant Name'].str.replace('cAMP-dependent protein kinase catalytic subunit alpha', 'KAPCA_BOVIN') # TODO move to creation of df
-        df_pair['Mutant Name'] = df_pair['Mutant Name'].str.replace('Epidermal growth factor receptor', 'EGFR_HUMAN') # TODO move to creation of df
+            df_pair = pd.read_csv(path)
 
-        # Add every mutant to the plot
-        for mutant_name in df_pair['Mutant Name'].unique():
-            df_subset = df_pair[df_pair['Mutant Name'] == mutant_name]
-            trace = go.Scatter(
-                x=df_subset['Positions'],
-                y=df_subset['IC50 Ratio'],
-                mode='markers',
-                name=mutant_name,
-                hovertemplate=(
-                    "<b>Position:</b> %{x}<br>"
-                    "<b>IC50 Ratio:</b> %{y}<br>"
-                    "<b>Mutation:</b> %{customdata}<br>"
-                ),
-                text=df_subset['Mutant Name'],
-                customdata=df_subset['Mutation'],
+            # Add traces for the current file
+            visibility = []
+            df_pair['Mutant Name'] = df_pair['Mutant Name'].str.replace('cAMP-dependent protein kinase catalytic subunit alpha', 'KAPCA_BOVIN') # TODO move to creation of df
+            df_pair['Mutant Name'] = df_pair['Mutant Name'].str.replace('Epidermal growth factor receptor', 'EGFR') # TODO move to creation of df
+            
+            # Add every mutant to the plot
+            for mutant_name in df_pair['Mutant Name'].unique():
+                df_subset = df_pair[df_pair['Mutant Name'] == mutant_name]
+                trace1 = go.Scatter(
+                    x=df_subset['Positions'],
+                    y=df_subset['IC50 Log Ratio'],
+                    mode='markers',
+                    name=mutant_name,
+                    hovertemplate=(
+                        "<b>Position:</b> %{x}<br>"
+                        "<b>IC50 Log Ratio:</b> %{y}<br>"
+                        "<b>Mutation:</b> %{customdata}<br>"
+                    ),
+                    text=df_subset['Mutant Name'],
+                    customdata=df_subset['Mutation'],
+                    
+                    visible=False,
+                    
+                )
+                df_subset_no_deletions = df_subset[df_subset.Type !='gap']
+                trace2 = go.Scatter(
+                    x=df_subset_no_deletions['Positions'],
+                    y=df_subset_no_deletions['IC50 Log Ratio'],  # Replace with your actual parameter
+                    mode='markers',
+                    hovertemplate=(
+                        "<b>Position:</b> %{x}<br>"
+                        "<b>IC50 Log Ratio:</b> %{y}<br>"
+                        "<b>Mutation:</b> %{customdata}<br>"
+                    ),
+                    name=mutant_name,
+                    customdata=df_subset_no_deletions['Mutation'],
+                    visible=False,
+                    text=None,
+                    marker=dict(
+                        color=df_subset_no_deletions['Probability Difference'],  # Color by Other Parameter value
+                        coloraxis='coloraxis'  # Choose a color scale to represent intensity
+                    ),
+                    showlegend=False
+                )
+                all_traces.append(trace1)
+                all_traces.append(trace2)
+                visibility.extend([True, True])
+
+            start_index = len(all_traces)  - len(visibility)
+            end_index = len(all_traces)
+            file_visibility = [False] * total_number_of_mutants_to_display * 2
+            for i in range(start_index, end_index):
+                file_visibility[i] = True
                 
-                visible=False,
-                
-            )
-            all_traces.append(trace)
-            visibility.append(True)
 
-        # Create a button for the current file
-        # Determine visibility for the current file
-        start_index = len(all_traces)  - len(visibility)
-        end_index = len(all_traces)
-        file_visibility = [False] * total_number_of_mutants_to_display
-        for i in range(start_index, end_index):
-            file_visibility[i] = True
+            buttons.append({
+                'label': f'Interaction pair {idx+1}',
+                'method': 'update',
+                'args': [
+                    {'visible': file_visibility}
+                ]
+            })
 
-        buttons.append({
-            'label': f'Interaction pair {idx+1}',
-            'method': 'update',
-            'args': [
-                {'visible': file_visibility}
-            ]
-        })
-
-        for trace in all_traces:
-            trace.visible = False
-        for i in range(start_index, end_index):
-            all_traces[i].visible = True
+            for trace in all_traces:
+                trace.visible = False
+            for i in range(start_index, end_index):
+                all_traces[i].visible = True
 
     # Add all traces to the figure
-    fig = go.Figure(data=all_traces)
-
+    for idx, t in enumerate(all_traces):
+        fig.append_trace(t, row=(idx%2)+1, col=1)
     # Add dropdown menu
     fig.update_layout(
         updatemenus=[{
@@ -180,10 +219,14 @@ def generate_interactive_ic50_plot():
             'active': 4
         }],
         title='Ratio of IC50 values between WT and Mutants by position',
-        xaxis_title='Mutation Position',
-        yaxis_title='Ratio of IC50 values between WT and a given mutant',
         hovermode='closest'
     )
+
+    fig.update_layout(coloraxis=dict(
+            colorscale='RdBu',cmin=-1,cmax=1,  
+            colorbar=dict(len=0.5, y=0.2)
+        ))
+
 
     # Save the interactive plot
     fig.write_html(os.path.join(saving_directory, 'interactive_ic50_plot.html'))
@@ -245,20 +288,23 @@ amino_acid_dict = {
     'Y': 'Tyrosine'
 }
 
-# TBD
-
+"""
 # Data loading
+print("Started loading data")
 df_mutants = pd.read_csv('../data/mutants.csv')
 df_merged = pd.read_csv('../data/merged_df.csv')
 df_mutants['Target Names'] = df_mutants['Target Names'].apply(lambda x: ast.literal_eval(x))
 df_mutants['BindingDB Target Chain Sequence'] = df_mutants['BindingDB Target Chain Sequence'].apply(lambda x: ast.literal_eval(x))
-print("loaded data")
+print("Loaded data")
+
 
 # Generate dfs and plots
-interaction_pair_names = save_dfs_ligands(df_merged, df_mutants)
+interaction_pairs = save_dfs_ligands(df_merged, df_mutants)
+interaction_pairs.to_csv('../data/interaction_pairs.csv')
 print("Saved dfs")
-ligand_names = visualize_ligands(interaction_pair_names[:,0])
+visualize_ligands(interaction_pairs['Ligand SMILES'])
 print("Saved ligand representations")
 generate_interactive_ic50_plot()
 print("Saved interactive IC50 plot")
 # visualize mutants
+"""
